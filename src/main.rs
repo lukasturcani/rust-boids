341468d4ab4c2957bcff36015ce117ad1a129c46
Lukas Turcani <lukasturcani93@gmail.com>
2023-03-24 18:31:58 +0000

    Add cohesion

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ const SEPARATION_RADIUS: f32 = 5.0;
 const SEPARATION_COEFFICIENT: f32 = 1.0;
 const VISIBLE_RADIUS: f32 = SEPARATION_RADIUS + 5.0;
 const ALIGNMENT_COEFFICIENT: f32 = 1.0;
+const COHESION_COEFFICIENT: f32 = 1.0;
 
 struct Boids;
 
@@ -25,6 +26,7 @@ impl Plugin for Boids {
         app.add_startup_system(spawn_boids);
         app.add_system(clear_separation.in_schedule(CoreSchedule::FixedUpdate));
         app.add_system(clear_alignment.in_schedule(CoreSchedule::FixedUpdate));
+        app.add_system(clear_cohesion.in_schedule(CoreSchedule::FixedUpdate));
         app.add_system(
             calculate_separation
                 .after(clear_separation)
@@ -36,9 +38,15 @@ impl Plugin for Boids {
                 .in_schedule(CoreSchedule::FixedUpdate),
         );
         app.add_system(
+            calculate_cohesion
+                .after(clear_cohesion)
+                .in_schedule(CoreSchedule::FixedUpdate),
+        );
+        app.add_system(
             update_boid_velocity
                 .after(calculate_separation)
                 .after(calculate_alignment)
+                .after(calculate_cohesion)
                 .in_schedule(CoreSchedule::FixedUpdate),
         );
         app.add_system(
@@ -93,6 +101,21 @@ impl Alignment {
     }
 }
 
+#[derive(Component)]
+struct Cohesion {
+    translation_sum: Vec2,
+    num_neighbors: u16,
+}
+
+impl Cohesion {
+    fn new() -> Self {
+        Self {
+            translation_sum: Vec2::new(0., 0.),
+            num_neighbors: 0,
+        }
+    }
+}
+
 fn spawn_boids(
     mut generator: ResMut<Generator>,
     mut commands: Commands,
@@ -119,6 +142,7 @@ fn spawn_boids(
                 Velocity(velocity),
                 Separation::new(),
                 Alignment::new(),
+                Cohesion::new(),
             )
         })
         .take(NUM_BOIDS)
@@ -150,10 +174,15 @@ fn clear_alignment(mut boids: Query<&mut Alignment>) {
     }
 }
 
-fn calculate_separation(
-    mut boids: Query<(&Transform, &mut Separation)>,
-    _time_step: Res<FixedTime>,
-) {
+fn clear_cohesion(mut boids: Query<&mut Cohesion>) {
+    for mut alignment in boids.iter_mut() {
+        alignment.translation_sum.x = 0.;
+        alignment.translation_sum.y = 0.;
+        alignment.num_neighbors = 0;
+    }
+}
+
+fn calculate_separation(mut boids: Query<(&Transform, &mut Separation)>) {
     let mut combinations = boids.iter_combinations_mut();
     while let Some([(transform1, mut separation1), (transform2, mut separation2)]) =
         combinations.fetch_next()
@@ -168,10 +197,7 @@ fn calculate_separation(
     }
 }
 
-fn calculate_alignment(
-    mut boids: Query<(&Transform, &Velocity, &mut Alignment)>,
-    _time_step: Res<FixedTime>,
-) {
+fn calculate_alignment(mut boids: Query<(&Transform, &Velocity, &mut Alignment)>) {
     let mut combinations = boids.iter_combinations_mut();
     while let Some(
         [(transform1, velocity1, mut alignment1), (transform2, velocity2, mut alignment2)],
@@ -187,12 +213,43 @@ fn calculate_alignment(
     }
 }
 
-fn update_boid_velocity(mut boids: Query<(&Separation, &Alignment, &mut Velocity)>) {
-    for (separation, alignment, mut velocity) in boids.iter_mut() {
+fn calculate_cohesion(mut boids: Query<(&Transform, &mut Cohesion)>) {
+    let mut combinations = boids.iter_combinations_mut();
+    while let Some([(transform1, mut cohesion1), (transform2, mut cohesion2)]) =
+        combinations.fetch_next()
+    {
+        let displacement = transform1.translation - transform2.translation;
+        if displacement.length() > SEPARATION_RADIUS && displacement.length() < VISIBLE_RADIUS {
+            cohesion1.translation_sum.x += transform2.translation.x;
+            cohesion1.translation_sum.y += transform2.translation.y;
+            cohesion2.translation_sum.x += transform1.translation.x;
+            cohesion2.translation_sum.y += transform1.translation.y;
+            cohesion1.num_neighbors += 1;
+            cohesion2.num_neighbors += 1;
+        }
+    }
+}
+
+fn update_boid_velocity(
+    mut boids: Query<(
+        &Transform,
+        &Separation,
+        &Alignment,
+        &Cohesion,
+        &mut Velocity,
+    )>,
+) {
+    for (transform, separation, alignment, cohesion, mut velocity) in boids.iter_mut() {
         if alignment.num_neighbors > 0 {
             let velocity_update =
                 (alignment.velocity_sum / (alignment.num_neighbors as f32)) - velocity.0;
             velocity.0 += velocity_update * ALIGNMENT_COEFFICIENT;
+        }
+        if cohesion.num_neighbors > 0 {
+            let mut velocity_update = cohesion.translation_sum / (cohesion.num_neighbors as f32);
+            velocity_update.x -= transform.translation.x;
+            velocity_update.y -= transform.translation.y;
+            velocity.0 += velocity_update * COHESION_COEFFICIENT;
         }
         velocity.0 += separation.displacment_sum * SEPARATION_COEFFICIENT;
     }
