a9d2a9f2da6ba4ec1d795fb8267fbc20da2c7711
Lukas Turcani <lukasturcani93@gmail.com>
2023-03-24 18:15:04 +0000

    Add alignment

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ const BOX_SIZE: f32 = 100.0;
 const MAX_BOID_VELOCITY: f32 = 10.0;
 const SEPARATION_RADIUS: f32 = 5.0;
 const SEPARATION_COEFFICIENT: f32 = 1.0;
+const VISIBLE_RADIUS: f32 = SEPARATION_RADIUS + 5.0;
+const ALIGNMENT_COEFFICIENT: f32 = 1.0;
 
 struct Boids;
 
@@ -22,14 +24,21 @@ impl Plugin for Boids {
         app.add_startup_system(setup);
         app.add_startup_system(spawn_boids);
         app.add_system(clear_separation.in_schedule(CoreSchedule::FixedUpdate));
+        app.add_system(clear_alignment.in_schedule(CoreSchedule::FixedUpdate));
         app.add_system(
             calculate_separation
                 .after(clear_separation)
                 .in_schedule(CoreSchedule::FixedUpdate),
         );
         app.add_system(
+            calculate_alignment
+                .after(clear_alignment)
+                .in_schedule(CoreSchedule::FixedUpdate),
+        );
+        app.add_system(
             update_boid_velocity
                 .after(calculate_separation)
+                .after(calculate_alignment)
                 .in_schedule(CoreSchedule::FixedUpdate),
         );
         app.add_system(
@@ -69,6 +78,21 @@ impl Separation {
     }
 }
 
+#[derive(Component)]
+struct Alignment {
+    velocity_sum: Vec2,
+    num_neighbors: u16,
+}
+
+impl Alignment {
+    fn new() -> Self {
+        Self {
+            velocity_sum: Vec2::new(0., 0.),
+            num_neighbors: 0,
+        }
+    }
+}
+
 fn spawn_boids(
     mut generator: ResMut<Generator>,
     mut commands: Commands,
@@ -94,6 +118,7 @@ fn spawn_boids(
                 },
                 Velocity(velocity),
                 Separation::new(),
+                Alignment::new(),
             )
         })
         .take(NUM_BOIDS)
@@ -117,6 +142,14 @@ fn clear_separation(mut boids: Query<&mut Separation>) {
     }
 }
 
+fn clear_alignment(mut boids: Query<&mut Alignment>) {
+    for mut alignment in boids.iter_mut() {
+        alignment.velocity_sum.x = 0.;
+        alignment.velocity_sum.y = 0.;
+        alignment.num_neighbors = 0;
+    }
+}
+
 fn calculate_separation(
     mut boids: Query<(&Transform, &mut Separation)>,
     _time_step: Res<FixedTime>,
@@ -135,8 +168,32 @@ fn calculate_separation(
     }
 }
 
-fn update_boid_velocity(mut boids: Query<(&mut Velocity, &Separation)>) {
-    for (mut velocity, separation) in boids.iter_mut() {
+fn calculate_alignment(
+    mut boids: Query<(&Transform, &Velocity, &mut Alignment)>,
+    _time_step: Res<FixedTime>,
+) {
+    let mut combinations = boids.iter_combinations_mut();
+    while let Some(
+        [(transform1, velocity1, mut alignment1), (transform2, velocity2, mut alignment2)],
+    ) = combinations.fetch_next()
+    {
+        let displacement = transform1.translation - transform2.translation;
+        if displacement.length() > SEPARATION_RADIUS && displacement.length() < VISIBLE_RADIUS {
+            alignment1.velocity_sum += velocity2.0;
+            alignment2.velocity_sum += velocity1.0;
+            alignment1.num_neighbors += 1;
+            alignment2.num_neighbors += 1;
+        }
+    }
+}
+
+fn update_boid_velocity(mut boids: Query<(&Separation, &Alignment, &mut Velocity)>) {
+    for (separation, alignment, mut velocity) in boids.iter_mut() {
+        if alignment.num_neighbors > 0 {
+            let velocity_update =
+                (alignment.velocity_sum / (alignment.num_neighbors as f32)) - velocity.0;
+            velocity.0 += velocity_update * ALIGNMENT_COEFFICIENT;
+        }
         velocity.0 += separation.displacment_sum * SEPARATION_COEFFICIENT;
     }
 }
