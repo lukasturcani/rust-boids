b4afa9f3ae434e2bb1391f34b0db526fcaccaabb
Lukas Turcani <lukasturcani93@gmail.com>
2023-04-04 21:29:41 +0100

    Use resources for parameters

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,6 @@ const RIGHT_MARGIN: f32 = BOX_SIZE * 0.5;
 const TOP_MARGIN: f32 = RIGHT_MARGIN;
 const BOTTOM_MARGIN: f32 = LEFT_MARGIN;
 
-const MAX_BOID_SPEED: f32 = 60.0;
-const MIN_BOID_SPEED: f32 = 15.0;
-const BOID_SPEED_RANGE: f32 = MAX_BOID_SPEED - MIN_BOID_SPEED;
-const SEPARATION_RADIUS: f32 = 3.0;
-const SEPARATION_COEFFICIENT: f32 = 0.1;
-const VISIBLE_RADIUS: f32 = 6.0;
-const ALIGNMENT_COEFFICIENT: f32 = 0.005;
-const COHESION_COEFFICIENT: f32 = 0.0005;
-const BOX_BOUND_COEFFICIENT: f32 = 0.2;
-
 struct Boids;
 
 impl Plugin for Boids {
@@ -32,6 +22,16 @@ impl Plugin for Boids {
         app.add_plugin(EguiPlugin);
         app.insert_resource(FixedTime::new_from_secs(BOID_TIMESTEP));
         app.insert_resource(Generator(StdRng::seed_from_u64(55)));
+        app.insert_resource(MaxBoidSpeed(60.0));
+        app.insert_resource(MinBoidSpeed(15.0));
+        app.insert_resource(SeparationRadius(3.0));
+        app.insert_resource(SeparationCoefficient(3.0));
+        app.insert_resource(SeparationCoefficient(0.1));
+        app.insert_resource(VisibleRadius(6.0));
+        app.insert_resource(AlignmentCoefficient(0.005));
+        app.insert_resource(CohesionCoefficient(0.0005));
+        app.insert_resource(BoxBoundCoefficient(0.2));
+        app.insert_resource(CameraScale(0.11));
         app.add_startup_system(setup);
         app.add_startup_system(spawn_boids);
         app.add_system(ui);
@@ -71,15 +71,42 @@ impl Plugin for Boids {
 #[derive(Resource)]
 struct Generator(pub StdRng);
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, scale: Res<CameraScale>) {
     commands.spawn(Camera2dBundle {
         projection: OrthographicProjection {
-            scale: 0.11,
+            scale: scale.0,
             ..default()
         },
         ..default()
     });
 }
+
+#[derive(Resource)]
+struct CameraScale(f32);
+
+#[derive(Resource)]
+struct MaxBoidSpeed(f32);
+
+#[derive(Resource)]
+struct MinBoidSpeed(f32);
+
+#[derive(Resource)]
+struct SeparationRadius(f32);
+
+#[derive(Resource)]
+struct SeparationCoefficient(f32);
+
+#[derive(Resource)]
+struct VisibleRadius(f32);
+
+#[derive(Resource)]
+struct AlignmentCoefficient(f32);
+
+#[derive(Resource)]
+struct CohesionCoefficient(f32);
+
+#[derive(Resource)]
+struct BoxBoundCoefficient(f32);
 
 #[derive(Component)]
 struct Velocity(Vec2);
@@ -129,6 +156,8 @@ impl Cohesion {
 
 fn spawn_boids(
     mut generator: ResMut<Generator>,
+    max_speed: Res<MaxBoidSpeed>,
+    min_speed: Res<MinBoidSpeed>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -137,8 +166,8 @@ fn spawn_boids(
         .sample_iter(Standard)
         .map(|[x, y, vx, vy, vmag]: [f32; 5]| {
             let velocity_direction = (Vec2::new(vx, vy) - 0.5).normalize();
-            let velocity =
-                velocity_direction * MIN_BOID_SPEED + velocity_direction * vmag * BOID_SPEED_RANGE;
+            let velocity = velocity_direction * min_speed.0
+                + velocity_direction * vmag * (max_speed.0 - min_speed.0);
             (
                 MaterialMesh2dBundle {
                     transform: Transform {
@@ -195,13 +224,16 @@ fn clear_cohesion(mut boids: Query<&mut Cohesion>) {
     }
 }
 
-fn calculate_separation(mut boids: Query<(&Transform, &mut Separation)>) {
+fn calculate_separation(
+    separation_radius: Res<SeparationRadius>,
+    mut boids: Query<(&Transform, &mut Separation)>,
+) {
     let mut combinations = boids.iter_combinations_mut();
     while let Some([(transform1, mut separation1), (transform2, mut separation2)]) =
         combinations.fetch_next()
     {
         let displacement = transform1.translation - transform2.translation;
-        if displacement.length() < SEPARATION_RADIUS {
+        if displacement.length() < separation_radius.0 {
             separation1.displacment_sum.x += displacement.x;
             separation1.displacment_sum.y += displacement.y;
             separation2.displacment_sum.x -= displacement.x;
@@ -210,14 +242,18 @@ fn calculate_separation(mut boids: Query<(&Transform, &mut Separation)>) {
     }
 }
 
-fn calculate_alignment(mut boids: Query<(&Transform, &Velocity, &mut Alignment)>) {
+fn calculate_alignment(
+    separation_radius: Res<SeparationRadius>,
+    visible_radius: Res<VisibleRadius>,
+    mut boids: Query<(&Transform, &Velocity, &mut Alignment)>,
+) {
     let mut combinations = boids.iter_combinations_mut();
     while let Some(
         [(transform1, velocity1, mut alignment1), (transform2, velocity2, mut alignment2)],
     ) = combinations.fetch_next()
     {
         let displacement = transform1.translation - transform2.translation;
-        if displacement.length() > SEPARATION_RADIUS && displacement.length() < VISIBLE_RADIUS {
+        if displacement.length() > separation_radius.0 && displacement.length() < visible_radius.0 {
             alignment1.velocity_sum += velocity2.0;
             alignment2.velocity_sum += velocity1.0;
             alignment1.num_neighbors += 1;
@@ -226,13 +262,17 @@ fn calculate_alignment(mut boids: Query<(&Transform, &Velocity, &mut Alignment)>
     }
 }
 
-fn calculate_cohesion(mut boids: Query<(&Transform, &mut Cohesion)>) {
+fn calculate_cohesion(
+    separation_radius: Res<SeparationRadius>,
+    visible_radius: Res<VisibleRadius>,
+    mut boids: Query<(&Transform, &mut Cohesion)>,
+) {
     let mut combinations = boids.iter_combinations_mut();
     while let Some([(transform1, mut cohesion1), (transform2, mut cohesion2)]) =
         combinations.fetch_next()
     {
         let displacement = transform1.translation - transform2.translation;
-        if displacement.length() > SEPARATION_RADIUS && displacement.length() < VISIBLE_RADIUS {
+        if displacement.length() > separation_radius.0 && displacement.length() < visible_radius.0 {
             cohesion1.translation_sum.x += transform2.translation.x;
             cohesion1.translation_sum.y += transform2.translation.y;
             cohesion2.translation_sum.x += transform1.translation.x;
@@ -244,6 +284,12 @@ fn calculate_cohesion(mut boids: Query<(&Transform, &mut Cohesion)>) {
 }
 
 fn update_boid_velocity(
+    min_speed: Res<MinBoidSpeed>,
+    max_speed: Res<MaxBoidSpeed>,
+    separation_coefficient: Res<SeparationCoefficient>,
+    alignment_coefficient: Res<AlignmentCoefficient>,
+    cohesion_coefficient: Res<CohesionCoefficient>,
+    box_bound_coefficient: Res<BoxBoundCoefficient>,
     mut boids: Query<(
         &Transform,
         &Separation,
@@ -256,28 +302,28 @@ fn update_boid_velocity(
         if alignment.num_neighbors > 0 {
             let velocity_update =
                 (alignment.velocity_sum / (alignment.num_neighbors as f32)) - velocity.0;
-            velocity.0 += velocity_update * ALIGNMENT_COEFFICIENT;
+            velocity.0 += velocity_update * alignment_coefficient.0;
         }
         if cohesion.num_neighbors > 0 {
             let mut velocity_update = cohesion.translation_sum / (cohesion.num_neighbors as f32);
             velocity_update.x -= transform.translation.x;
             velocity_update.y -= transform.translation.y;
-            velocity.0 += velocity_update * COHESION_COEFFICIENT;
+            velocity.0 += velocity_update * cohesion_coefficient.0;
         }
         if transform.translation.x < LEFT_MARGIN {
-            velocity.0.x += BOX_BOUND_COEFFICIENT;
+            velocity.0.x += box_bound_coefficient.0;
         }
         if transform.translation.x > RIGHT_MARGIN {
-            velocity.0.x -= BOX_BOUND_COEFFICIENT;
+            velocity.0.x -= box_bound_coefficient.0;
         }
         if transform.translation.y < BOTTOM_MARGIN {
-            velocity.0.y += BOX_BOUND_COEFFICIENT;
+            velocity.0.y += box_bound_coefficient.0;
         }
         if transform.translation.y > TOP_MARGIN {
-            velocity.0.y -= BOX_BOUND_COEFFICIENT;
+            velocity.0.y -= box_bound_coefficient.0;
         }
-        velocity.0 += separation.displacment_sum * SEPARATION_COEFFICIENT;
-        velocity.0 = velocity.0.clamp_length(MIN_BOID_SPEED, MAX_BOID_SPEED);
+        velocity.0 += separation.displacment_sum * separation_coefficient.0;
+        velocity.0 = velocity.0.clamp_length(min_speed.0, max_speed.0);
     }
 }
 
